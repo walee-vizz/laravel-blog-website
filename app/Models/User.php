c9c37a65f64ce97bad368c55d58c879e6116bbfa
walee-vizz <muhammadwaleedkhan052@gmail.com>
2023-12-18 22:52:53 +0500

    roles added, and user policy created only

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,20 +3,58 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Panel;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+
+
+
+    const ROLE_ADMIN = 'ADMIN';
+    const ROLE_EDITOR = 'EDITOR';
+    const ROLE_USER = 'USER';
+    const ROLE_DEFAULT = self::ROLE_USER;
+
+
+    const ROLES = [
+        self::ROLE_ADMIN => 'Admin',
+        self::ROLE_EDITOR => 'Editor',
+        self::ROLE_USER => 'User',
+    ];
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->isAdmin() || $this->isEditor();
+    }
+
+
+
+    public function isAdmin()
+    {
+        return $this->role == self::ROLE_ADMIN;
+    }
+
+    public function isEditor()
+    {
+        return $this->role == self::ROLE_EDITOR;
+    }
+
+
+
 
     /**
      * The attributes that are mass assignable.
@@ -27,6 +65,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -72,7 +111,8 @@ class User extends Authenticatable
         return $this->belongsToMany(Post::class, 'post_like')->withTimestamps();
     }
 
-    public function hasLiked(Post $post){
+    public function hasLiked(Post $post)
+    {
         return $this->likes()->where('post_id', $post->id)->exists();
     }
 }
