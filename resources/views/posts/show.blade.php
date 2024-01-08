d30896516c2b1a6ab76d1e846b9aa10284574072
walee-vizz <muhammadwaleedkhan052@gmail.com>
2024-01-08 23:06:37 +0500

    db file added

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -56,7 +56,6 @@
     </article>
     <div id="side-bar"
         class="border-t border-t-gray-100 md:border-t-none col-span-4 md:col-span-1 px-3 md:px-6  space-y-10 py-6 pt-10 md:border-l border-gray-100 h-screen sticky top-0 shrink">
-        <h1>Random Links</h1>
 
         @include('posts.partials.categories-box')
 
