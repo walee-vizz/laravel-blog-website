df5cdb0893e406cf667c96edf8ee5336fbf38317
walee-vizz <muhammadwaleedkhan052@gmail.com>
2024-01-09 15:31:09 +0500

    minor adjustments made

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     @section('hero')
         <div class="w-full text-center py-32">
             <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
-                Welcome to <span class="text-yellow-500">&lt;Neotronic Dev&gt;</span>
+                Welcome to
             </h1>
             <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
             <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
@@ -12,7 +12,7 @@
     @endsection
     <div class="mb-10">
         <div class="mb-16">
-            <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
+            <h2 class="mt-16 mb-5 text-3xl text-dark-500 font-bold">Featured Posts</h2>
             <div class="w-full">
                 <div class="grid grid-cols-3 gap-10 w-full">
                     @foreach ($featured_posts as $feature_post)
@@ -21,13 +21,13 @@
 
                 </div>
             </div>
-            <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold"
+            <a class="mt-10 block text-center text-lg text-dark-500 font-semibold"
                 href="{{ route('posts.index') }}">More
                 Posts</a>
         </div>
         <hr>
 
-        <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Latest Posts</h2>
+        <h2 class="mt-16 mb-5 text-3xl text-dark-500 font-bold">Latest Posts</h2>
         <div class="w-full mb-5">
             <div class="grid grid-cols-3 gap-10 w-full">
                 @foreach ($latest_posts as $latest_post)
@@ -35,7 +35,7 @@
                 @endforeach
             </div>
         </div>
-        <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold" href="{{ route('posts.index') }}">More
+        <a class="mt-10 block text-center text-lg text-dark-500 font-semibold" href="{{ route('posts.index') }}">More
             Posts</a>
     </div>
 
