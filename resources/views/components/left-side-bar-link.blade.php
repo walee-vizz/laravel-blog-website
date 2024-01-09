d525f34fdbecd84264da7548b2f568a92400d9f1
walee-vizz <muhammadwaleedkhan052@gmail.com>
2024-01-09 15:15:46 +0500

    minor adjustments made

--- resources/views/components/left-side-bar-link.blade.php
+++ resources/views/components/left-side-bar-link.blade.php
@@ -1,6 +1,6 @@
-@props(['text', 'icon'])
+@props(['text', 'icon', 'url'])
 <li class="border-rounded" style="background-color: #f6ece44f">
-    <a href="" {!! $attributes->merge([
+    <a href="{{ $url }}" {!! $attributes->merge([
         'class' =>
             'group/navigation-section-link block transition duration-300 rounded-lg px-4 py-3 font-display bg-merino/70 dark:bg-dolphin/20 font-semibold text-hurricane dark:text-merino hover:text-[#403b3b] hover:bg-merino dark:hover:bg-dolphin/30 dark:hover:text-white ',
     ]) !!} style="background-color: #f6ece44f">
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['active', 'navigate'])
 
 @php
-    $classes = $active ?? false ? 'inline-flex items-center' : 'inline-flex items-center hover:text-gray-900 text-sm text-yellow-500';
+    $classes = $active ?? false ? 'inline-flex items-center' : 'group/navigation-section-link block transition duration-300 rounded-lg px-4 py-3 font-display bg-merino/70 dark:bg-dolphin/20 font-semibold text-hurricane dark:text-merino hover:text-[#403b3b] hover:bg-merino dark:hover:bg-dolphin/30 dark:hover:text-white';
 @endphp
 
 <a {{ $navigate ?? true ? 'wire:navigate' : '' }} {{ $attributes->merge(['class' => $classes]) }}>
